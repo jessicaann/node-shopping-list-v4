94671bd2453054dd5b6daa35c4115839fb3be4e5
jessicaann <jessicaecottrell@gmail.com>
2017-06-18 16:27:47 -0700

    PUT endpoint Recipes

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/recipes', (req, res) => {
 });
 
 app.post('/recipes', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
+  // ensure `name` and `ingredients` are in request body
   const requiredFields = ['name', 'ingredients'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
@@ -109,6 +109,32 @@ app.post('/recipes', jsonParser, (req, res) => {
 app.delete('/recipes/:id', (req, res) => {
   Recipes.delete(req.params.id);
   console.log(`Deleted recipe \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating recipes item \`${req.params.id}\``);
+  Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
   res.status(204).end();
 });
 
